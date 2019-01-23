b0d28c081e8481f46f45189d462169777195b943
Sarah <sarah@sarahfrench.co.uk>
2019-01-23 18:14:37 +0000

    refactored to not need fs module

--- src/app.js
+++ src/app.js
@@ -1,5 +1,4 @@
 const sharp = require('sharp');
-const fs = require('fs');
 
 const roundedCorners = Buffer.from(
   '<svg><rect x="0" y="0" width="200" height="200" rx="100" ry="100" /></svg>'
@@ -9,13 +8,9 @@ function cropImgToCircle(imageName){
   sharp(imageName)
     .resize(200, 200)
     .overlayWith(roundedCorners, { cutout: true, gravity: 'centre' })
-    .toBuffer()
-    .then( data => {
-      fs.writeFileSync('output.png', data, 'utf8', (err) =>{
-        console.log('An error occurred, yay')
-      })
-    })
-    .catch( err => console.log(err) );
+    .toFile('output.jpg', function(err) {
+      console.log(err);
+  });
 }
 
 cropImgToCircle('input.jpg');
